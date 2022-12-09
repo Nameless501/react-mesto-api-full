589681054047d2a9f844378b1b20593a32c58a1a
Eugene Zorin <ezorin5@yandex.ru>
2022-12-09 19:11:01 +0400

    fix

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -7,6 +7,12 @@ const { requestLogger } = require('../middlewares/logger');
 
 router.use(requestLogger);
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', signinValidation, login);
 router.post('/signup', signupValidation, createUser);
 
